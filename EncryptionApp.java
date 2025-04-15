cacfb74656ebe8a19e7c03f383c6c00d83bc1681
Dane West <cameron.west676@topper.wku.edu>
2025-04-15 09:32:07 -0500

    Update EncryptionApp.java

--- EncryptionApp.java
+++ EncryptionApp.java
@@ -1,6 +1,7 @@
 import com.mathworks.engine.*;
 import java.util.Scanner;
 import java.util.concurrent.ExecutionException;
+import EncryptionHelper.java;
 
 public class EncryptionApp {
     public static void main (String[] args) throws InterruptedException, ExecutionException {
@@ -36,44 +37,5 @@ public class EncryptionApp {
         String F = modExp(C, a, n, engine);
         System.out.println("Decrypted message: " + F);
         // System.out.println(decimalToText(F, chunkSize, engine));
-    }
-
-    public static MatlabEngine engineStart() {
-        try {
-            //create and initialize a MATLAB engine object
-            MatlabEngine mEngine = MatlabEngine.startMatlab();
-            String path = System.getProperty("user.dir");
-            mEngine.eval("cd('" + path.replace("\\", "/") + "')");
-
-            return mEngine;
-            // Error handling for different problems
-        } catch (Exception e) {
-            System.err.println("Engine start failed: " + e.getMessage());
-        }
-        return null;
-    }
-    public static String modExp(String base, int exponent, int modulus, MatlabEngine eng) {
-        try {
-            return eng.feval("modExp", base, exponent, modulus);
-        } catch (InterruptedException | ExecutionException e) {
-            System.err.println("modExp failed: " + e.getMessage());
-        }
-        return "";
-    }
-    public static String textToDecimal(String text, int chunkSize, MatlabEngine eng) {
-        try {
-            return eng.feval("textToDecimal", text, chunkSize);
-        } catch (InterruptedException | ExecutionException e) {
-            System.err.println("textToDecimal failed: " + e.getMessage());
-        }
-        return "";
-    }
-    public static String decimalToText (String num, int chunkSize, MatlabEngine eng) {
-        try {
-            return eng.feval("decimalToText", num, chunkSize);
-        } catch (InterruptedException | ExecutionException e) {
-            System.err.println("decimalToText failed: " + e.getMessage());
-        }
-        return "";
     }
 }
