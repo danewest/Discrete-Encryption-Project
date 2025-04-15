13b193cc5469e0be7128714a6646c1e90d96f716
Dane West <cameron.west676@topper.wku.edu>
2025-04-15 09:30:29 -0500

    Update EncryptionHelper.java
    Added the necessary methods.

--- EncryptionHelper.java
+++ EncryptionHelper.java
@@ -1,3 +1,40 @@
 public class EncryptionHelper {
-  
+  public static MatlabEngine engineStart() {
+        try {
+            //create and initialize a MATLAB engine object
+            MatlabEngine mEngine = MatlabEngine.startMatlab();
+            String path = System.getProperty("user.dir");
+            mEngine.eval("cd('" + path.replace("\\", "/") + "')");
+
+            return mEngine;
+            // Error handling for different problems
+        } catch (Exception e) {
+            System.err.println("Engine start failed: " + e.getMessage());
+        }
+        return null;
+    }
+    public static String modExp(String base, int exponent, int modulus, MatlabEngine eng) {
+        try {
+            return eng.feval("modExp", base, exponent, modulus);
+        } catch (InterruptedException | ExecutionException e) {
+            System.err.println("modExp failed: " + e.getMessage());
+        }
+        return "";
+    }
+    public static String textToDecimal(String text, int chunkSize, MatlabEngine eng) {
+        try {
+            return eng.feval("textToDecimal", text, chunkSize);
+        } catch (InterruptedException | ExecutionException e) {
+            System.err.println("textToDecimal failed: " + e.getMessage());
+        }
+        return "";
+    }
+    public static String decimalToText (String num, int chunkSize, MatlabEngine eng) {
+        try {
+            return eng.feval("decimalToText", num, chunkSize);
+        } catch (InterruptedException | ExecutionException e) {
+            System.err.println("decimalToText failed: " + e.getMessage());
+        }
+        return "";
+    }
 }
