66c9adf37f8e4194c0f6e541b3f391564f9822ce
Dane West <cameron.west676@topper.wku.edu>
2025-04-15 11:23:10 -0500

    Created package for all to be contained, helps with importing from EncryptionHelper.java

--- encryption/EncryptionApp.java
+++ encryption/EncryptionApp.java
@@ -1,6 +1,9 @@
+package encryption;
+
 import com.mathworks.engine.*;
 import java.util.Scanner;
 import java.util.concurrent.ExecutionException;
+import encryption.EncryptionHelper;
 
 public class EncryptionApp {
     public static void main (String[] args) throws InterruptedException, ExecutionException {
@@ -21,19 +24,21 @@ public class EncryptionApp {
         String input = in.nextLine();
 
         //start MATLAB
-        MatlabEngine engine = engineStart();
+        System.out.println("Starting MATLAB engine...");
+        MatlabEngine engine = EncryptionHelper.engineStart();
+        System.out.println("MATLAB engine started.");
 
         // encrypt message M
         System.out.println("Calling textToDecimal...");
-        String M = textToDecimal(input, chunkSize, engine);
+        String M = EncryptionHelper.textToDecimal(input, chunkSize, engine);
         System.out.println("Message into decimal: " + M);
-        String C = modExp(M, b, n, engine); // message is encrypted
+        String C = EncryptionHelper.modExp(M, b, n, engine); // message is encrypted
         System.out.println("Encrypted message: " + C);
         // System.out.println(decimalToText(C, chunkSize, engine)); // prints encrypted message
         System.out.println("");
 
         //decrypt message C
-        String F = modExp(C, a, n, engine);
+        String F = EncryptionHelper.modExp(C, a, n, engine);
         System.out.println("Decrypted message: " + F);
         // System.out.println(decimalToText(F, chunkSize, engine));
     }
