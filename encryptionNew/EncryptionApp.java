e3424bb7d18ae17c3c54612535c2f966a92d4bcb
danewest <cameron.west676@topper.wku.edu>
2025-04-17 23:53:33 -0500

    Finished implementing the program.

--- encryptionNew/EncryptionApp.java
+++ encryptionNew/EncryptionApp.java
@@ -9,7 +9,6 @@ public class EncryptionApp {
     public static void main (String[] args) throws InterruptedException, ExecutionException {
         // establishing necessary variables, *these will not change!*
         final BigInteger p, q, one, n, phi, e, d;
-        final int chunkSize = 3;
         p = new BigInteger("61");
         q = new BigInteger("53");
         one = new BigInteger("1");
@@ -17,7 +16,7 @@ public class EncryptionApp {
         n = p.multiply(q); //multiply
         phi = (p.subtract(one)).multiply(q.subtract(one)); // Euler's totient
 
-        e = new BigInteger("3533"); // public
+        e = new BigInteger("1993"); // public
         d = e.modInverse(phi);
         //d = 291481949; // private key
 
@@ -27,10 +26,8 @@ public class EncryptionApp {
         System.out.println("What is your message?");
         String input = in.nextLine();
 
-        String encrypted = encrypt(input, e, n);
-        System.out.println(encrypted);
+        String[] encrypted = encrypt(input, e, n);
 
         String decrypted = decrypt(encrypted, d, n);
-        System.out.println(decrypted);
     }
 }
--- encryptionNew/EncryptionHelper.java
+++ encryptionNew/EncryptionHelper.java
@@ -3,88 +3,134 @@ package encryptionNew;
 import java.math.BigInteger;
 
 public class EncryptionHelper {
-    // establishing necessary variables, *these will not change!*
+    private static final String ALPHABET = "abcdefghijklmnopqrstuvwxyz ";
+    private static final int BASE = ALPHABET.length(); // 27
 
     // Checks for the first occurrence of target char in string. If it doesn't appear the result is -1/false
-    public static int isCharInString(char target, String sourceString) {
+    private static int isCharInString(char target, String sourceString) {
         return sourceString.indexOf(target);
     }
+
     // Translates given strings into decimal format
-    public static String textToDecimal(String inText) {
+    private static String textToDecimal(String inText) {
         String text = inText.toLowerCase();
-        String alphabet = "abcdefghijklmnopqrstuvwxyz ";
         String resString = "";
         
         // Checks if the character is in the alphabet. If it is
         for (int i = 0; i < text.length(); i++) {
-            int curr = isCharInString(text.charAt(i), alphabet);
+            int curr = isCharInString(text.charAt(i), ALPHABET);
             if (curr != -1) {
+                //resString += curr;
                 resString += String.format("%02d", curr);
             } else {
                 break;
             }
         }
-
         System.out.println(inText + " -> " + resString);
         // Converts the converted text into an integer and returns it
         return resString;
     }
 
-    // Translates decrypted numbers back into english
-    public static String decimalToText(String num) {
-        String alphabet = "abcdefghijklmnopqrstuvwxyz ";
-        String resString = "";
+    // Translates decrypted numbers back into english (for Strings)
+    private static String decimalToText(String num) {
+        StringBuilder res = new StringBuilder();
 
-        // Looks for the index that each character is associated with
-        for (int i = 0; i < num.length(); i++) {
-            char index = isCharInString();
-            resString += alphabet.charAt(index);
+        for (int i = 0; i < num.length(); i += 2) {
+            String chunk = num.substring(i, i + 2);
+            int index = Integer.parseInt(chunk);
+            res.append(ALPHABET.charAt(index));
         }
 
-        System.out.println(num + "-> " + resString);
-        return resString;
+        System.out.println(num + " -> " + res.toString());
+        return res.toString();
     }
 
-    public static String encrypt(String inText, BigInteger exponent, BigInteger mult) {
-        String res = "";
+    // Converts encrypted numbers into gibberish text
+    private static String numberToFakeText(BigInteger num) {
+        StringBuilder res = new StringBuilder();
+        BigInteger base = BigInteger.valueOf(BASE);
 
-        // If the length of the text is not divisible by 3 add spaces until it does
-        if (inText.length() % 3 != 0) {
-            for (int i = 0; i < inText.length() % 3; i++) {
-                inText += " ";
+        int temp = num.intValue();
+
+        if (num.equals(BigInteger.ZERO)) {
+            return String.valueOf(ALPHABET.charAt(0));
+        }
+
+        while (num.compareTo(BigInteger.ZERO) > 0) {
+            BigInteger[] divRem = num.divideAndRemainder(base);
+            res.insert(0, ALPHABET.charAt(divRem[1].intValue()));
+            num = divRem[0];
+        }
+
+        return res.toString();
+    }
+
+    // Converts gibberish text back into a number to be decrypted
+    private static BigInteger fakeTextToNumber(String text) {
+        BigInteger result = BigInteger.ZERO;
+        BigInteger base = BigInteger.valueOf(BASE);
+
+        for (int i = 0; i < text.length(); i++) {
+            int index = isCharInString(text.charAt(i), ALPHABET);
+            if (index == -1) {
+                throw new IllegalArgumentException("Invalid character in encrypted text: " + text.charAt(i));
             }
+            result = result.multiply(base).add(BigInteger.valueOf(index));
         }
 
-        // Convert the text with spaces into numbers
-        String num = textToDecimal(inText);
+        //System.out.println(text + " to " + result);
+        return result;
+    }
 
-        // Encrypt the numbered text in 3 part chunks, then put those chunks together in the result
-        for (int i = 0; i < num.length(); i+=3) {
-            String sub = num.substring(i, i + 3);
-
-            int textNum = Integer.parseInt(sub);
-
-            BigInteger text = BigInteger.valueOf(textNum);
-            BigInteger cipher = text.modPow(exponent, mult);
-
-            res += cipher.toString();
+    // Encrypts given string
+    public static String[] encrypt(String inText, BigInteger exponent, BigInteger mult) {
+        // If the length of the text is not divisible by 3 add spaces until it does
+        if (inText.length() % 2 != 0) {
+            inText += " ";
         }
-        System.out.println(res);
-        res = decimalToText(res);
-        System.out.println(inText + " -> " + res);
+
+        // String array for the chunks & counter for the loop
+        String[] res = new String[inText.length() / 2];
+        int resCount = 0;
+
+        for (int i = 0; i < inText.length(); i += 2) {
+            String chunk = inText.substring(i, i + 2);
+            String decimal = textToDecimal(chunk);
+
+            BigInteger plainNum = new BigInteger(decimal);
+            BigInteger encrypted = plainNum.modPow(exponent, mult);
+            res[resCount++] = numberToFakeText(encrypted);
+        }
+
+        arrayPrint(res);
         return res;
     }
 
-    public static String decrypt(String cipher, BigInteger exponent, BigInteger mult) {
-        cipher = textToDecimal(cipher);
-        int textNum = Integer.parseInt(cipher);
+    // Decrypts the number given by fakeTextToNumber
+    public static String decrypt(String[] cipher, BigInteger exponent, BigInteger mult) {
+        StringBuilder res = new StringBuilder();
 
-        BigInteger text = BigInteger.valueOf(textNum);
-        BigInteger decryptedNum = text.modPow(exponent, mult);
+        for (int i = 0; i < cipher.length; i++) {
+            BigInteger encodedNum = fakeTextToNumber(cipher[i]);
+            BigInteger decryptedNum = encodedNum.modPow(exponent, mult);
+            String numStr = decryptedNum.toString();
 
-        String decrypted = decimalToText(decryptedNum.toString());
+            // Each chunk was 2 letters → 2 digits each = 4-digit total
+            while (numStr.length() < 4) numStr = "0" + numStr;
 
-        System.out.println(cipher + " -> " + decrypted);
-        return decrypted.toString();
+            res.append(decimalToText(numStr));
+        }
+
+        System.out.println("Decrypted text: " + res.toString());
+        return res.toString();
+    }
+
+    // Prints chunked messages. For testing
+    private static void arrayPrint(String[] message) {
+        System.out.print("\nMessage: ");
+        for (int i = 0; i < message.length; i++) {
+            System.out.print(message[i]);
+        }
+        System.out.println("\n");
     }
 }
