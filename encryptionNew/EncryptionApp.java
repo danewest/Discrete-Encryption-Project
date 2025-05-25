c62ac0202aeb79c45001d3e3557807b15dc578b3
Emily Kedenburg <emily.kedenburg@gmail.com>
2025-05-25 17:26:59 -0500

    Update EncryptionApp.java
    Removed temporary comment code

--- encryptionNew/EncryptionApp.java
+++ encryptionNew/EncryptionApp.java
@@ -9,19 +9,6 @@ import java.util.concurrent.ExecutionException;
 public class EncryptionApp {
     public static BigInteger p, q, n, phi, e, d;
     public static void main (String[] args) throws InterruptedException, ExecutionException {
-        // establishing necessary variables, *these will not change!*
-        //final BigInteger p, q, one, n, phi, e, d;
-        //p = new BigInteger("61");
-        //q = new BigInteger("53");
-        //one = new BigInteger("1");
-
-        //n = p.multiply(q); //multiply
-        //phi = (p.subtract(one)).multiply(q.subtract(one)); // Euler's totient
-
-        //e = new BigInteger("1993"); // public
-        //d = e.modInverse(phi);
-        //d = 291481949; // private key
-
         Scanner in = new Scanner(System.in);
 
         // prompt user to choose default or custom variables
@@ -50,7 +37,6 @@ public class EncryptionApp {
                            "p=" + p + " q=" + q + " n=" + n + " phi=" + phi + " e=" + e + " d=" + d + "\n");
 
         System.out.println("What is your message?");
-        //in.nextLine();
         String input = in.nextLine();
 
         String[] encrypted = encrypt(input, e, n);
